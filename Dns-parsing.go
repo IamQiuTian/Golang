d10cfdf62979f3ec92156c207b9438aba3fec217
IamQiuTian <devopsvip@163.com>
2017-06-06 14:08:03 +0800

    批量域名解析成IP

--- Dns-parsing.go
+++ Dns-parsing.go
@@ -32,7 +32,12 @@ func main() {
 
 	for _, v := range url {
 		result, _ := dnsurL(v)
-		fmt.Println(v, ": ", result)
+		if result != nil {
+			fmt.Println("[+] ", v, ": ", result)
+		} else {
+			fmt.Println("[-] ", v, ": ", "Did not find dns parsing records")
+
+		}
 	}
 }
 
@@ -64,25 +69,18 @@ func dnsurL(uL string) ([]string, error) {
 }
 
 /*
-www.nmgjxw.gov.cn :  [222.74.213.236]
-www.nmggzw.gov.cn :  [110.16.70.8]
-nmgfy.chinacourt.org :  [111.202.173.230]
-ssmzggbm.nmgov.edu.cn :  [101.7.0.40]
-nmdswx.nmds.gov.cn :  [218.21.128.235]
-www.nmg.gov.cn :  [60.31.197.41]
-www.ordos.gov.cn :  [58.18.251.58]
-www.nmschoolfootball.com :  [101.7.0.75]
-zypt.nmgov.edu.cn :  [101.7.0.42]
-nmgmwzy.nmgov.edu.cn :  [101.7.0.43]
-nmgs.gov.cn :  [116.112.10.219 222.74.203.130]
-neimenggu.mca.gov.cn :  [116.193.41.42]
-www.nmds.gov.cn :  [218.21.128.234]
-www.nmagri.gov.cn :  [110.16.70.147]
-www.nmgat.gov.cn :  [58.18.164.36]
-www.nmfda.gov.cn :  [110.16.70.10]
-www.nmgcz.gov.cn :  [202.99.230.232]
-www.nmgepb.gov.cn :  [202.99.246.243]
-www.nmgfgw.gov.cn :  [61.138.111.230]
-www.nmggtt.gov.cn :  [116.114.18.210]
-www.nmgjrb.gov.cn :  [110.16.70.32]
+[+]  www.btjxw.gov.cn :  [1.24.191.100]
+[+]  www.bynrjxw.gov.cn :  [221.199.203.239]
+[-]  www.als.gov.cn/jw :  Did not find dns parsing records
+[+]  jw.xlgl.gov.cn :  [110.16.70.58]
+[+]  jjj.elht.gov.cn :  [222.74.213.233]
+[+]  www.wlcbjxw.gov.cn :  [124.67.110.69]
+[+]  jw.xam.gov.cn :  [58.18.7.197 58.18.7.202]
+[+]  www.hlbrjxw.gov.cn :  [58.18.185.134]
+[+]  www.nmgwh.gov.cn :  [110.16.70.8]
+[+]  www.nmgxwcbgdj.gov.cn :  [222.74.213.250 110.16.70.105]
+[+]  www.bjnmghotel.com :  [115.29.108.91]
+[+]  www.xincheng-hotel.com.cn :  [121.41.231.83]
+[+]  www.hhhtbtjc.com :  [42.123.76.89]
+[+]  www.hctrust.cn :  [162.251.95.52 162.251.95.54]
 */
