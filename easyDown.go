58871d8fff83add8021e8067c1d8df448bb61554
Your Name <you@example.com>
2017-10-14 22:31:03 +0800

    目录游览及文件下载

--- easyDown.go
+++ easyDown.go
@@ -2,42 +2,41 @@ package main
 
 import (
 	"flag"
-    "time"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
+	"path/filepath"
 	"strings"
-    "path/filepath"
+	"time"
 
 	"github.com/yosssi/go-fileserver"
 )
 
 var (
 	directory *string = flag.String("d", "", "Directory path")
-	file *string = flag.String("f", "", "file path")
+	file      *string = flag.String("f", "", "file path")
 	port      *string = flag.String("p", "8888", "Listening port")
 )
 
-
 func main() {
 	flag.Parse()
-	if len(*directory) == 0 &&  len(*file) == 0 {
+	if len(*directory) == 0 && len(*file) == 0 {
 		flag.Usage()
 		return
 	}
-    if len(*file) != 0 {
-        filename := filepath.Base(*file)
-        dirpath := filepath.Dir(*file)
-        *directory = dirpath
-        getPublic(*port, filename)
-        getPrivate(*port, filename)
-    } else {
-        filename := "false"
-        getPublic(*port, filename)
-        getPrivate(*port, filename)
-    }
+	if len(*file) != 0 {
+		filename := filepath.Base(*file)
+		dirpath := filepath.Dir(*file)
+		*directory = dirpath
+		getPublic(*port, filename)
+		getPrivate(*port, filename)
+	} else {
+		filename := "false"
+		getPublic(*port, filename)
+		getPrivate(*port, filename)
+	}
 	fs := fileserver.New(fileserver.Options{})
 	http.Handle("/", fs.Serve(http.Dir(*directory)))
 	if err := http.ListenAndServe(":"+*port, nil); err != nil {
@@ -45,12 +44,11 @@ func main() {
 	}
 }
 
-
 func getPublic(port, filename string) {
-    timeout := time.Duration(1 * time.Second)
-    client := http.Client{
-        Timeout: timeout,
-    }
+	timeout := time.Duration(1 * time.Second)
+	client := http.Client{
+		Timeout: timeout,
+	}
 	resp, err := client.Get("http://members.3322.org/dyndns/getip")
 	if err != nil {
 		return
@@ -60,26 +58,26 @@ func getPublic(port, filename string) {
 	if err != nil {
 		log.Fatal(err)
 	}
-    if filename == "false" {
-        fmt.Printf("Tour address: http://%s:%s\n", strings.Replace(string(b), "\n", "", -1), port)
-    } else {
-        fmt.Printf("Tour address: http://%s:%s/%s\n", strings.Replace(string(b), "\n", "", -1), port, filename)
-    }
+	if filename == "false" {
+		fmt.Printf("Tour address: http://%s:%s\n", strings.Replace(string(b), "\n", "", -1), port)
+	} else {
+		fmt.Printf("Tour address: http://%s:%s/%s\n", strings.Replace(string(b), "\n", "", -1), port, filename)
+	}
 }
 
 func getPrivate(port, filename string) {
 	addrs, err := net.InterfaceAddrs()
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 	for _, address := range addrs {
 		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
 			if ipnet.IP.To4() != nil {
-                if filename == "false" {
-                    fmt.Printf("Tour address: http://%s:%s\n", ipnet.IP.String(), port)
-                } else {
-                    fmt.Printf("Tour address: http://%s:%s/%s\n", ipnet.IP.String(), port, filename)
-                }
+				if filename == "false" {
+					fmt.Printf("Tour address: http://%s:%s\n", ipnet.IP.String(), port)
+				} else {
+					fmt.Printf("Tour address: http://%s:%s/%s\n", ipnet.IP.String(), port, filename)
+				}
 			}
 		}
 	}
