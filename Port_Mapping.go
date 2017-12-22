b6d583ebeb68e183a264bb15c165c82582900d40
iamqiutian <qiudays@163.com>
2017-12-22 10:23:45 +0800

    Port Mapping

--- Port_Mapping.go
+++ Port_Mapping.go
@@ -36,27 +36,6 @@ func (r *route) handle(src net.Conn) {
 		log.Printf("Error connecting to destination: %s\n", r.dest)
 		return
 	}
-func main() {
-	s := flag.String("s", "nil", "Listened ip")
-	d := flag.String("d", "nil", "Mapping ip")
-	flag.Parse()
-
-	if *s == "nil" || *d == "nil" {
-		flag.Usage()
-		return
-	}
-
-	r := &route{
-		src:  *s,
-		dest: *d,
-	}
-	go r.listen()
-	<-chan bool(nil)
-}
-
-func (r *route) handle(src net.Conn) {
-	dst, err := net.Dial("tcp", r.dest)
-	if err != nil {
 
 	log.Printf("Routing %s -> %s", r.src, r.dest)
 
@@ -79,6 +58,6 @@ func (r *route) listen() {
 		if err != nil {
 			log.Printf("Error accepting connection: %s", err)
 		}
-		go r.handle(conn) 
+		go r.handle(conn)
 	}
 }
