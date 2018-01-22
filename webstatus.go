9f0862cc4234683c434a7287eb8b6e9a1cb8bb8a
iamqiutian <qiudays@163.com>
2018-01-22 15:58:16 +0800

    Website survives

--- webstatus.go
+++ webstatus.go
@@ -2,32 +2,36 @@ package main
 
 import (
 	"bufio"
-	"errors"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"runtime"
 	"strings"
+	"sync"
+	"time"
 )
 
-func main() {
-	var filepath string
-	flag.StringVar(&filepath, "f", "url.txt", "-f <filename>")
-	flag.Parse()
-
-	url := openfile(filepath)
-	for _, v := range url {
-		uL, status, err := urL(v)
-		if status != 200 && err != nil {
-			fmt.Printf("%v 不可以访问\n", uL)
-		}
-		fmt.Printf("%v 可以访问\n", uL)
-	}
+func init() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
 }
 
-func openfile(filename string) []string {
+func main() {
+	filepath := flag.String("f", "url.txt", "-f <filename>")
+	flag.Parse()
+
+	wg := sync.WaitGroup{}
+	url := Openfile(*filepath)
+	wg.Add(len(url))
+	for _, v := range url {
+		go UrL(&wg, v)
+	}
+	wg.Wait()
+}
+
+func Openfile(filename string) []string {
 	var lo []string
 	f, err := os.Open(filename)
 	if err != nil {
@@ -45,31 +49,31 @@ func openfile(filename string) []string {
 	return lo
 }
 
-func urL(uL string) (string, int, error) {
+func UrL(wg *sync.WaitGroup, uL string) {
 	u, err := url.Parse(uL)
 	if err != nil {
 		log.Println(err)
 	}
 	q := u.Query()
 	u.RawQuery = q.Encode()
-	res, err := http.Get(u.String())
+
+	timeout := time.Duration(1 * time.Second)
+	client := http.Client{
+		Timeout: timeout,
+	}
+	res, err := client.Get(u.String())
 	if err != nil {
-		return uL, 0, errors.New("not open")
+		fmt.Printf("%v Connect failed\n", uL)
+		wg.Done()
+		return
 	}
 	resCode := res.StatusCode
 	defer res.Body.Close()
 
-	return uL, resCode, nil
+	if resCode != 200 {
+		fmt.Printf("%v Connect failed\n", uL)
+	} else {
+		fmt.Printf("%v Connect Success\n", uL)
+	}
+	wg.Done()
 }
-
-/*
-http://222.74.213.247 可以访问
-http://110.16.70.81 可以访问
-http://116.113.100.77 可以访问
-http://110.16.70.3 可以访问
-http://110.16.70.26 可以访问
-http://110.16.70.69 可以访问
-http://110.16.70.70 可以访问
-http://110.16.70.71 不可以访问
-http://110.16.70.71 可以访问
-*/
